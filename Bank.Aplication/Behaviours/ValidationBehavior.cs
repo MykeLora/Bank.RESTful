e2f0068143217d6ace6bc16a6143201f61e6e48c
MykeLora <maycollora16@gmail.com>
2024-10-03 17:23:02 -0600

    Implementando la lógica para manejar los request y los response utilizando pipelineBehavior.

---
+++ Bank.Aplication/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Aplication.Behaviours
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TResponse>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TResponse>> validator)
+        {
+            _validators = validator;
+            
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new FluentValidation.ValidationContext<TRequest>(request);
+                var validatorResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validatorResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new Exceptions.ValidationException(failures);
+                     
+                    
+            }
+            return await next();
+        }
+    }
+}
