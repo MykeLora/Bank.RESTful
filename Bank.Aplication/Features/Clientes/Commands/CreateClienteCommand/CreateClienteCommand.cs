3c560bd0ff1665f9df7fb5162872f1e580114338
MykeLora <maycollora16@gmail.com>
2024-10-04 22:01:26 -0600

    Estructuración de los folder para la implementación de CQRS

---
+++ Bank.Aplication/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
@@ -0,0 +1,14 @@
+﻿using Bank.Aplication.Wrappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Aplication.Features.Clientes.Commands.CreateClienteCommand
+{
+    public class CreateClienteCommand : IRequest<Response<int>>
+    {
+    }
+}
