4ca2de0e3e244bb027d730745e85f26ad04e525d
MykeLora <maycollora16@gmail.com>
2024-10-04 22:04:31 -0600

    Agregando los campos que se necesitan para el mapeo con la entidad cliente.

--- Bank.Aplication/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
+++ Bank.Aplication/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommand.cs
@@ -10,5 +10,12 @@ namespace Bank.Aplication.Features.Clientes.Commands.CreateClienteCommand
 {
     public class CreateClienteCommand : IRequest<Response<int>>
     {
+        public string Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string? Direccion { get; set; }
+
     }
 }
