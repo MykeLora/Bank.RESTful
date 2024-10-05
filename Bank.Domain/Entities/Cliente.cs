8cbfd6ed9bc50fe0182e15c4158513c5bb724534
MykeLora <maycollora16@gmail.com>
2024-10-04 21:02:00 -0600

    Lógica para cálcular la edad del atributo FechaNacimiento.

--- Bank.Domain/Entities/Cliente.cs
+++ Bank.Domain/Entities/Cliente.cs
@@ -9,11 +9,25 @@ namespace Bank.Domain.Entities
 {
     public class Cliente : BaseEntity
     {
+        private int _Edad;
         public string Nombre { get; set; }
         public string? Apellido { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public string  Telefono { get; set; }
         public string Email { get; set; }
         public string? Direccion { get; set; }
+
+        public int Edad
+        {
+            get
+            {
+                if(this._Edad  <= 0)
+                {
+                    this._Edad = new DateTime(DateTime.Now.Subtract(this.FechaNacimiento).Ticks).Year - 1;
+                }
+                return this._Edad;
+            }
+
+        }
     }
 }
