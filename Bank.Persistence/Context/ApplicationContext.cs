e6906510c9f89498ad32e7e2a781695ce83e0c3b
MykeLora <maycollora16@gmail.com>
2024-10-05 00:00:57 -0600

    Haciéndo la implementación del patrón repositorio para trabajar entidades de manera génerica.

---
+++ Bank.Persistence/Context/ApplicationContext.cs
@@ -0,0 +1,43 @@
+﻿using Bank.Aplication.Interfaces;
+using Bank.Domain.Common;
+using Bank.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly IDateTimeService _dateTime;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options, IDateTimeService dateTime) : base(options
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            _dateTime = dateTime;
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.Created = _dateTime.NowUtc;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModified = _dateTime.NowUtc;
+                        break;
+                }
+
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Bank.Persistence/Core/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Bank.Aplication.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.Core
+{
+    public abstract class BaseRepository<T> : RepositoryBase<T>, IBaseRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+
+        public BaseRepository(DbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
