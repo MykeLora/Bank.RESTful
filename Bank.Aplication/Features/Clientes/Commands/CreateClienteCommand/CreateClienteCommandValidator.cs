a7b5e25d1739dd22204d5d2c876a9abcc9af0aa3
MykeLora <maycollora16@gmail.com>
2024-10-08 23:02:12 -0600

    Validando los campos del comando createCliente utilizando fluent validator.

---
+++ Bank.Aplication/Features/Clientes/Commands/CreateClienteCommand/CreateClienteCommandValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Aplication.Features.Clientes.Commands.CreateClienteCommand
+{
+    public class CreateClienteCommandValidator : AbstractValidator<CreateClienteCommand>
+    {
+        public CreateClienteCommandValidator()
+        {
+            RuleFor(n => n.Nombre)
+                    .NotEmpty().WithMessage("{PropertyName} the property Name required")
+                    .MaximumLength(80).WithMessage("{PropertyName} should not exceed of {MaxLength} ");
+
+            RuleFor(n => n.Apellido)
+                    .NotEmpty().WithMessage("{PropertyName} the property Name required")
+                    .MaximumLength(80).WithMessage("{PropertyName} should not exceed of {MaxLength} ");
+
+            RuleFor(n => n.FechaNacimiento)
+                    .NotEmpty().WithMessage("{PropertyName} the property cannot empty");
+
+            RuleFor(n => n.Telefono)
+                    .NotEmpty().WithMessage("{PropertyName} the property Name required")
+                    .Matches(@"^\d{4}-\d{4}$").WithMessage("{PropertyName} should be in format 0000-0000")
+                    .MaximumLength(9).WithMessage("{PropertyName} should not exceed of {MaxLength} ");
+
+            RuleFor(n => n.Email)
+                    .NotEmpty().WithMessage("{PropertyName} the property Name required")
+                    .EmailAddress().WithMessage("{PropertyName} should be an adreesse valid ")
+                    .MaximumLength(100).WithMessage("{PropertyName} should not exceed of {MaxLength} ");
+
+            RuleFor(n => n.Direccion)
+                    .NotEmpty().WithMessage("{PropertyName} the property Name required")
+                    .MaximumLength(120).WithMessage("{PropertyName} should not exceed of {MaxLength} ");
+
+        }
+    }
+}
